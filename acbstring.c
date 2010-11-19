86750cd5a2050443378666c261096f4333c4314f
Spike Grobstein <spikegrobstein@mac.com>
2010-11-19 08:39:59 -0800

    fixed a bug in acbstring.c where I was using the length of the string rather than number of bytes for the conversion. this was resulting in fewer characters being converted from the strings.

--- acbstring.c
+++ acbstring.c
@@ -9,22 +9,9 @@ void acb_string_to_utf8(ACB_String *string, char *buf) {
 	iconv_t cd = iconv_open("ISO-8859-1", "UTF-16");
 	
 	size_t outbytes = string->length;
-	size_t inbytes = string->length;
+	size_t inbytes = string->length * 2;
 	
 	size_t icv = iconv(cd, &string->string, &inbytes, &buf, &outbytes);
 	
 	iconv_close(cd);
-	
-	
-	/*
-	bzero(utf8_buf, string->length + 1);
-	UTF8 *utf8TargetStart = utf8_buf;
-	
-	UTF16 *utf16SourceStart, *utf16SourceEnd;
-	utf16SourceStart = string->string;
-	
-	ConversionResult r = ConvertUTF16toUTF8((const UTF16**) &utf16SourceStart, &(string->string[string->length * 2]), &utf8TargetStart, &(utf8_buf[string->length * 2]), strictConversion);
-	
-	printf("result: %d\n", r);
-	*/
 }
