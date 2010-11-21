9a2f62e086f6ef23fa7ebdb787c921152d723aa7
Spike Grobstein <spikegrobstein@mac.com>
2010-11-21 02:23:02 -0800

    moved some color-related defines into acbcolorrecord.h, fixing some compile issues

--- acbutils.h
+++ acbutils.h
@@ -29,28 +29,6 @@
 #define ACB_SIGNATURE 			"8BCB"
 #define ACB_VERSION					0x0001
 
-// color record stuff
-#define ACB_COLOR_RECORD_CODE_SIZE	6 /* number of bytes in the color_code field */
-
-//colorspace constants:
-#define ACB_CS_RGB				0
-#define	ACB_CS_CMYK				2
-#define	ACB_CS_LAB				7
-
-//colorspace constants (unimplemented by adobe? unsupported?):
-#define ACB_CS_HSB				1
-#define	ACB_CS_PANTONE		3
-#define ACB_CS_FOCALTONE	4
-#define ACB_CS_TRUMATCH		5
-#define ACB_CS_TOYO				8
-#define ACB_CS_GRAYSCALE	8
-#define ACB_CS_HKS				10
-
-// colorspace datasize in bytes
-#define ACB_CS_RGB_SIZE		3
-#define ACB_CS_CMYK_SIZE	4
-#define ACB_CS_LAB_SIZE		3
-
 // other color spaces are not defined? who knows what size they should be.
 
 typedef struct acb_record {
